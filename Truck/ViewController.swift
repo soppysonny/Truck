4c8023b0c673f870bb284b0c64da7074c48bb4ea
zhangming <1213980685@qq.com>
2020-09-24 14:06:02 +0800

    Initial Commit

---
+++ Truck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Truck
+//
+//  Created by zhangming on 2020/9/24.
+//  Copyright © 2020 Binada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
